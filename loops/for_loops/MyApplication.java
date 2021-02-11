7985803dc88d8dcd902bec78b022e98652d5129b
safe1801 <sadinho88@hotmail.com>
2021-02-11 21:14:46 +0100

    Forloops sets and reps.

---
+++ loops/for_loops/MyApplication.java
@@ -0,0 +1,15 @@
+public class MyApplication {
+	public static void main(String[]args){
+        // if you run it like this you will have an infinite loop
+		for(;;){
+			
+			// Whats being executed
+
+			System.out.println("I love my Cat");
+
+
+			}
+
+		} 
+
+	}
---
+++ loops/for_loops/MyApplication_2.java
@@ -0,0 +1,14 @@
+public class MyApplication {
+	public static void main(String[]args){
+		
+
+	for(int i=0;i<10;i=i+1){  // this format will always return true if you leave the paranthesis empty. To enable control of the loop you need to (intialize a variable; create a condition such as setting a threshold; and adding a update section where the value inside the variable changes on every iteration.
+
+			System.out.println("I love my Cat");
+
+
+			}
+
+		} 
+
+	}

57325057cad110fda5b1eaa1c7f34bcf05eda01c
safe1801 <sadinho88@hotmail.com>
2021-02-11 22:51:58 +0100

    .

--- loops/for_loops/MyApplication_2.java
+++ loops/for_loops/MyApplication_2.java
@@ -2,9 +2,9 @@ public class MyApplication {
 	public static void main(String[]args){
 		
 
-	for(int i=0;i<10;i=i+1){  // this format will always return true if you leave the paranthesis empty. To enable control of the loop you need to (intialize a variable; create a condition such as setting a threshold; and adding a update section where the value inside the variable changes on every iteration.
+	for(int i=0;i<10;i++) {  // this format will always return true if you leave the paranthesis empty. To enable control of the loop you need to (intialize a variable; create a condition such as setting a threshold; and adding an update section where the value inside the variable changes on every iteration until we have a false condition.
 
-			System.out.println("I love my Cat");
+			System.out.printf("Format specifier contains a special character, begins with percentage sign, following with a letter. Integer values use the d letter : %d.\n", i);
 
 
 			}
